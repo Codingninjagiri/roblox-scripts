97e92b2d089e688bbfa5c0c1acb496f4cf0cc9ca
Codingninjagiri <fiskerjensenspark@gmail.com>
2024-10-25 16:53:24 +0200

    Create skidflinglaaa.lua

---
+++ skidflinglaaa.lua
@@ -0,0 +1,5 @@
+while true do
+  wait(1)
+  print("reexecuted")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Codingninjagiri/roblox-scripts/refs/heads/main/SkidFling.lua",true))()
+end
